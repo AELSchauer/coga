7f39b3c75db5b8f7f529f0a93d5e0be74a39ca9e
Ashley Schauer <aelschauer@gmail.com>
2018-11-26 16:40:44 -0700

    reset app again to fix rails s error

--- spec/factories/users.rb
+++
@@ -1,9 +0,0 @@
-FactoryBot.define do
-  factory :user do
-    username { 'JoeSchmoe' }
-    first_name { 'Joe' }
-    last_name { 'Schmoe' }
-    company_email { 'joe.schmoe@company.com' }
-    roles { nil }
-  end
-end
